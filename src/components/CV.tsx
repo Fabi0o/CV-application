2cfdc0057320b9489fb5e0f77d3a5e4c00261f17
Fabi0o <marfab32@gmail.com>
2023-05-04 17:04:38 +0200

    add components folder

---
+++ src/components/CV.tsx
@@ -0,0 +1,5 @@
+const CV = () => {
+  return <div></div>;
+};
+
+export default CV;
---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+const Form = () => {
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input type="text" />
+    </form>
+  );
+};
+
+export default Form;
