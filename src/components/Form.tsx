ed21e6358b2ec35fb262cd1c77bf5c2bac3cd689
Fabi0o <marfab32@gmail.com>
2023-05-13 10:21:33 +0200

    add form styling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,5 @@
+import style from "../style/Form.module.scss";
+
 type Props = {
   setFirstName: React.Dispatch<React.SetStateAction<string>>;
   setLastName: React.Dispatch<React.SetStateAction<string>>;
@@ -12,33 +14,44 @@ const Form: React.FC<Props> = ({
   setBio,
 }) => {
   return (
-    <form>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        type="text"
-        id="firstName"
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        type="text"
-        id="lastName"
-        onChange={(e) => setLastName(e.target.value)}
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label htmlFor="bio">Bio</label>
-      <textarea
-        name="bio"
-        id=""
-        cols={30}
-        rows={10}
-        onChange={(e) => setBio(e.target.value)}
-      ></textarea>
+    <form className={style.container}>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="bio">Bio</label>
+        <textarea
+          name="bio"
+          id=""
+          cols={30}
+          rows={10}
+          onChange={(e) => setBio(e.target.value)}
+        ></textarea>
+      </div>
     </form>
   );
 };
